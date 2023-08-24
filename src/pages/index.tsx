19e36bb961a8ceeadd306a451ee2cf1b89de06ee
Chacha <chachazw@gmail.com>
2023-08-24 11:44:01 +0800

    feat: add postcss.config.js, update home page styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,90 +1,40 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import styles from "@/styles/Home.module.css";
+import { Button, Link } from "@nextui-org/react";
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Nota - An AI Music NFT marketplace</title>
-        <meta name="description" content="Nota - An AI Music NFT marketplace where you can create amazing music NFT with AI and trade your NFT with music lovers around the world!" />
+        <meta
+          name="description"
+          content="Nota - An AI Music NFT marketplace where you can create amazing music NFT with AI and trade your NFT with music lovers around the world!"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                src="/logo.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={40}
-                height={40}
-                priority
-              />
-               Nota
-            </a>
-          </div>
+          <Link href="/" rel="noopener noreferrer" className="flex gap-2">
+            <Image
+              src="/logo.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={40}
+              height={40}
+              priority
+            />
+            <p className="text-black">Nota</p>
+          </Link>
+          <Button color="primary">Connect Wallet</Button>
         </div>
 
-        <div className={styles.center}>
-          <div className={styles.thirteen}>
-            <p>Mint Music NFT</p>
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              About <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about AI Music NFT in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Team <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and know more about our team.
-            </p>
-          </a>
-
-          <a
-            href="https://www.riffusion.com/about"
-            className={styles.card}  
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              How it works? <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about AI Music NFT features.
-            </p>
-          </a>
-        </div>
+        <Button className={`${styles.thirteen} mb-10`}>
+          <p>Mint Music NFT</p>
+        </Button>
       </main>
     </>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+
 const { nextui } = require("@nextui-org/react");
 
 module.exports = {
   content: [
+    "./pages/**/*.{ js, ts, jsx, tsx}",
+    "./components/**/*.{ js, ts, jsx, tsx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [nextui()],
-}
-
+  darkMode: "class",
+  plugins: [
+    nextui({
+      prefix: "nextui", // prefix for themes variables
+      addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+      defaultTheme: "light", // default theme from the themes object
+      defaultExtendTheme: "light", // default theme to extend on custom themes
+      layout: {}, // common layout tokens (applied to all themes)
+      themes: {
+        light: {
+          layout: {}, // light theme layout tokens
+          colors: {
+            primary: "#FA709A",
+          }, // light theme colors
+        },
+        dark: {
+          layout: {}, // dark theme layout tokens
+          colors: {}, // dark theme colors
+        },
+        // ... custom themes
+      },
+    }),
+  ],
+};
